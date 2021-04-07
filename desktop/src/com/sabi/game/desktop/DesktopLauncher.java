0aa54af60eae7ad298b4dd0633ae906e51776427
Radhakrishnan <sradhakrishnan@teksystems.com>
2021-04-07 14:16:41 -0500

    First Commit; Boiler platecode

---
+++ desktop/src/com/sabi/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sabi.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sabi.game.StarFishCollector;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarFishCollector(), config);
+	}
+}
